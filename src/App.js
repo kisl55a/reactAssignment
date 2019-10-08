2b6477ca89032c2477dc50ab89944f8dde3ce5b8
Dmitrii <kislenko1910@mail.ru>
2019-10-08 12:59:46 +0300

    Added change of location by clicking

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, {PropTypes, Component} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import GoogleMapReact from 'google-map-react';
 import Marker from './components/Marker';
 import styles from './components/Main.module.css';
+import shouldPureComponentUpdate from 'react-pure-render/function';
 
+import controllable from 'react-controllables';
 
+// @controllable(['center', 'zoom', 'hoverKey', 'clickKey'])
+// var React = require('react');
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      center: {
+        lat: 59.95,
+        lng: 30.33
+      },
+      zoom: 11,
       Markers : [ 
         {
           lat: 59.955413, 
@@ -24,24 +34,22 @@ export default class App extends Component {
         },
       ]
     }
-    
   };
   // TODO find out how to change the location
-  // static propTypes = {
-  //   onCenterChange: PropTypes.func, // @controllable generated fn
-  // };
+ 
 
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
-  _onClick = ({x, y, lat, lng, event}) => console.log(x, y, lat, lng, event)
+
+
+  // _onClick = ({x, y, lat, lng, event}) => console.log(x, y, lat, lng, event)
+
   _onChildClick = (key, childProps) => {
-    console.log(childProps);
-    // this.props.onCenterChange([childProps.lat, childProps.lng]);
+    this.setState({
+      center: {
+        lat: childProps.lat,
+        lng: childProps.lng
+      },
+      zoom: 11
+    })
   }
   render() {
     return (
@@ -51,24 +59,17 @@ export default class App extends Component {
         </div>
         <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
+          center={this.state.center}
           bootstrapURLKeys={{ key: "AIzaSyBQc4fDzvIrxXU2Md73EjyY6oXWspFCMSY" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          onClick = { this._onClick }
-          onChildClick = { this._onChildClick }
+          zoom={this.state.zoom}
+          onBoundsChange={this._onBoundsChange}
+          onChildClick={this._onChildClick}
         >
           {this.state.Markers.map((item, i) => (
-            <Marker key={i} lat = { item.lat} lng = { item.lng} text ={ item.text }/>
-           
+            <Marker key={i} lat = { item.lat } lng = { item.lng } text ={ item.text }/>
             ))
         }
         {console.log(this.state)}
-          {/* <Marker
-          
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          /> */}
         </GoogleMapReact>
       </div>
       </div>
