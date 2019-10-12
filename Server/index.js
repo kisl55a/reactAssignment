3a0b4dae44adfdb5c471d6f6738b5a4881ab71dd
Dmitrii Kislenko <kislenko1910@mail.ru>
2019-10-12 12:19:26 +0300

    Connected to DB

---
+++ Server/index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const app = express();
+
+const port = 4000;
+const bodyParser = require('body-parser');
+const miceData = require('./data/mice.json');
+var cors = require('cors');
+const db = require('./db');
+
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+app.get('/getData', (req, res) =>{
+    db.query('SELECT * FROM stations').then(results => {
+        res.json(results)
+    })
+    .catch(() => {
+        res.sendStatus(500);
+    })  
+});
+app.post('/addData', (req, res) =>{
+//  отправлять промисы после выполнения лупы
+    let data = req.body;
+    Promise.all( [
+    data.forEach(element => {
+         db.query('INSERT INTO data (name, description, company, price, currency, ship, image) VALUES (?,?,?,?,?,?,?)', [element.name, element.description, element.company, element.price, element.currency, element.ship, element.image])
+    })]
+    ).then((response) => {
+        res.send('succesfull');
+    })
+    .catch((err) => {
+        console.log(err);
+        // res.send(err);
+    })          
+});
+app.patch('/changeData', (req, res) =>{
+    //  отправлять промисы после выполнения лупы
+        let data = req.body;
+        Promise.all( [
+        data.forEach(element => {
+             db.query('UPDATE data SET name = (?), description = (?) , company = (?), price = (?), currency = (?), ship = (?), image = (?) WHERE data.id =(?)', [element.name, element.description, element.company, element.price, element.currency, element.ship, element.image, element.id])
+        })]
+        ).then((response) => {
+            res.send('succesfull');
+        })
+        .catch((err) => {
+            console.log(err);
+            // res.send(err);
+        })          
+    });
+// Do not accept "delete" request
+app.patch('/deleteData', (req, res) =>{
+        //  отправлять промисы после выполнения лупы
+            let data = req.body;
+            // console.log(data);
+            Promise.all( [
+            data.forEach(element => {
+                 db.query('DELETE FROM `data` WHERE `data`.`id` = (?)', [element.id])
+            })]
+            ).then((response) => {
+                res.send('succesfull');
+            })
+            .catch((err) => {
+                console.log(err);
+                // res.send(err);
+            })          
+        });
+// DB init 
+Promise.all(    
+    [
+        db.query("CREATE TABLE IF NOT EXISTS stations(`stationId` INT NOT NULL AUTO_INCREMENT , `stationName` TEXT NOT NULL , `address` TEXT NOT NULL ,`lat` INT NOT NULL , `lng` INT NOT NULL, `type` TEXT NOT NULL , `price` INT NOT NULL , `measure` TEXT NOT NULL , PRIMARY KEY (`stationId`))")
+        // Add more table create statements if you need more tables
+    ]
+).then(() => {
+    console.log('database initialized');
+    app.listen(port, () => {
+           console.log('Listening to port ', port)
+
+    });
+});
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './components/Marker';
+import axios from 'axios';
 import styles from './components/Main.module.css';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import MainPage from './components/MainPage'
@@ -9,27 +10,31 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      data: [],
       currentMarker: {},
       center: {
-        lat: 59.95,
-        lng: 30.33
+        lat: 65.01,
+        lng: 25.49
       },
-      zoom: 11,
+      zoom: 15,
       Markers: [
-        {
-          lat: 59.955413,
-          lng: 30.337844,
-          text: "da"
-        },
-        {
-          lat: 58.955413,
-          lng: 30.337844,
-          text: "da",
-          available: false
-        },
       ]
     }
   };
+
+  componentDidMount = () =>
+  {    
+    axios.get('http://localhost:4000/getData').then(result => {
+      this.setState({Markers: result.data})
+      console.log(this.state.Markers);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }
+  onDrag = ((map) => {
+    console.log('dsadasd')
+  })
   _onChildClick = (key, childProps) => {
     console.log(this.state.currentMarker)
     this.setState({
@@ -37,7 +42,7 @@ export default class App extends Component {
         lat: childProps.lat,
         lng: childProps.lng
       },
-      zoom: 15,
+      zoom: 14,
       currentMarker: childProps,
     })
   }
@@ -51,7 +56,6 @@ export default class App extends Component {
           (routeProps) =>
             <MainPage
               currentMarker={ this.state.currentMarker }
-             
               />
         } />
         </Router>
@@ -65,8 +69,9 @@ export default class App extends Component {
             onBoundsChange={this._onBoundsChange}
             onChildClick={this._onChildClick}
           >
+            { console.log(this.state.zoom)}
             {this.state.Markers.map((item, i) => (
-              <Marker key={i} lat={item.lat} lng={item.lng} text={item.text} />
+              <Marker key={i} lat={item.lat} lng={item.lng} text={item.stationName} />
             ))
             }
           </GoogleMapReact>
