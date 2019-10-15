d9dd815231301f53cdf073b9ccc0318e865354c8
Dmitrii Kislenko <kislenko1910@mail.ru>
2019-10-15 10:08:16 +0300

    Added current data not to miss it

---
+++ Server/data/data.js
@@ -0,0 +1,22 @@
+[
+    {
+        "stationId": 1,
+        "stationName": "Prisma Raksilla",
+        "address": "Tehtaankatu 3, 90130 Oulu",
+        "lat": 65.01062,
+        "lng": 25.490793,
+        "type": "Fast",
+        "price": "0",
+        "measure": "e"
+    },
+    {
+        "stationId": 3,
+        "stationName": "Peltola",
+        "address": "Peltolankaari 6,\r\n90230 Oulu",
+        "lat": 65.012615,
+        "lng": 25.51618,
+        "type": "Slow",
+        "price": "0",
+        "measure": "c/kWh"
+    }
+]
