0d5ec308900d9d9611d97ac51ff3a345403ca23f
dmitrii <kislenko1910@mail.ru>
2020-01-20 19:26:26 +0200

    Small changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 4000 || process.env.PORT;
 const bodyParser = require('body-parser');
 var cors = require('cors');
 const db = require('./db');
